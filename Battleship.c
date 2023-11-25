2f1eca63ed389aa9bc7c1981d58e501be6ed9a51
ElectricalKin <151864331+ElectricalKin@users.noreply.github.com>
2023-11-25 08:16:08 -0500

    Update Battleship.c

--- Battleship.c
+++ Battleship.c
@@ -1,5 +1,22 @@
-//Project Partners: Reemal Hoor, Kineret Ortega EECE 210 Software Engineering 1 Dr.Wafa Elmannai 
+/* Project Partners: Reemal Hoor, Kineret Ortega 
+EECE 210 Software Engineering 1
+Dr.Wafa Elmannai */
 
 
-#include <iostream>
+#include <stdio.h>
+int main()
+{
+  //Print statments to start game
+Printf("Enter the name of the input file now: ");
+Printf("Initializing board....");
+Printf("Game board has been initialized....");
+Printf("Placing boats onto the game board....");
+Printf("=====> Current Board Status <====="); 
+  //can change line 16 to a loop instead with increments to make more advanced
+Printf("0  1  2  3  4  5  6  7  8  9");
+
+
+  
+}
+
 
